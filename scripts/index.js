a44a65d64dadbbcc4e2dcd6b346b00ef21ffa116
Yuriy <barkov1981yu@yandex.ru>
2022-04-18 03:36:29 +0200

    Исправления готовы

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,31 @@
 const editButton = document.querySelector('.profile__edit-button'),
   modalWindow = document.querySelector('.popup'),
   modalCloseBtn = modalWindow.querySelector('.popup__close'),
-  nameInput = document.querySelector('.popup__name'),
-  jobInput = document.querySelector('.popup__job'),
+  nameInput = document.querySelector('.popup__profile_name'),
+  jobInput = document.querySelector('.popup__profile_job'),
   profileName = document.querySelector('.profile__name'),
-  profileJob = document.querySelector('.profile__job'),
-  groupElement = document.querySelector('.group');
+  profileJob = document.querySelector('.profile__job');
 
 let formElement = document.querySelector('form');
 
-nameInput.value = profileName.innerText;
-jobInput.value = profileJob.innerText;
-
 function formSubmitHandler (event) {
   event.preventDefault();
+
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   toggleModalWindow();
-}
-
-function toggleFill(event) {
-  let el = event.target;
-  if (el.classList.contains('element__like')) {
-    if (el.classList.contains('active')){
-      el.setAttribute('src', 'images/like.svg');
-    }
-    else {
-      el.setAttribute('src', 'images/Union.svg');
-    }
-    el.classList.toggle('active');
-  }
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
 function toggleModalWindow() {
   modalWindow.classList.toggle('popup_opened');
-  nameInput.focus();
+  if(modalWindow.classList.contains('popup_opened')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    nameInput.focus();
+  }
 }
 
 editButton.addEventListener('click', toggleModalWindow);
-modalCloseBtn.addEventListener('click', toggleModalWindow);
-groupElement.addEventListener('click', toggleFill);
+modalCloseBtn.addEventListener('click', toggleModalWindow)
